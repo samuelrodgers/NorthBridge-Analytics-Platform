8e39760776be9200f6af5d0ffc96e17b40a5a8ba
Samuel Rodgers <samuel.rodgers.24@cnu.edu>
2026-03-14 13:04:31 -0400

    Count Companies query
    I have to start with a simple one to make sure things are working, then there are some database issues that need to be resolved before the complicated charts can be added.

--- scripts/SQL/Scripts/Superset_Queries.sql
+++ scripts/SQL/Scripts/Superset_Queries.sql
@@ -6,3 +6,14 @@
 -- Main TX
 -- Displays time-series data on total revenue per company
 -- Will include lots of dropdown options that are not described in this query
+
+-- There are issues with the database that need to be resolved before this chart can be made properly
+
+
+
+-- Number of Companies
+-- Displays the number of companies with data in the system
+-- Simple big number format, might add links later for quick navigation
+
+SELECT COUNT(c_id) AS total_companies
+FROM analytics.d_company;
