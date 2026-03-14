d98cad9b61b8bf58671015d615f3cb33c757fa0e
Samuel Rodgers <samuel.rodgers.24@cnu.edu>
2026-03-14 12:39:51 -0400

    Documenting queries used with Superset

---
+++ scripts/SQL/Scripts/Superset_Queries.sql
@@ -0,0 +1,8 @@
+-- This file contains all the queries for the frontend Superset charts. 
+-- Each query was handwritten by Sam Rodgers and reviewed by Claude AI.
+
+
+
+-- Main TX
+-- Displays time-series data on total revenue per company
+-- Will include lots of dropdown options that are not described in this query
