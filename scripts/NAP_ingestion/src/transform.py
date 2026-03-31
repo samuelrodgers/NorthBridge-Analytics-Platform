d76bba85c0dbd7269667f3a3b36cbfbd5a69ea6e
Samuel Rodgers <samuel.rodgers.24@cnu.edu>
2026-03-31 15:48:55 -0400

    try again with the timeout

--- scripts/NAP_ingestion/src/transform.py
+++ scripts/NAP_ingestion/src/transform.py
@@ -125,6 +125,10 @@ def get_connection():
         dbname=os.getenv("DB_NAME"),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASS"),
+        keepalives=1,
+        keepalives_idle=60,
+        keepalives_interval=10,
+        keepalives_count=5,
     )
 
 
